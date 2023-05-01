d77922c0e8930b9229cf93e22467363b5813f9c3
caitlynroddy <caitlyn_roddy@brown.edu>
2023-05-01 17:01:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
+import numpy as np #importing numpy as np to make the code easier to read and understand#
 
-def game_board():
-    empty_board = np.zeros((6,9))
-    return
+def game_board(): #game board function defined#
+    empty_board = np.zeros((6,9)) #create a 6 by 9 matrix of zeros as an empty board#
+    return board #return this as board#
+
